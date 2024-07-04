9e168efd07ab147a159832b3d566c62c9b1839b8
tienhnpl <dotien.21797@gmail.com>
2024-07-05 01:59:20 +0700

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,17 +5,43 @@ const register = asyncHandler(async (req, res) => {
   const { firstname, lastname, email, mobile, password } = req.body;
   if (!firstname || !lastname || !email || !mobile || !password) {
     return res.status(400).json({
-        success : false,
-        mes: 'Missing inputs...',
-    })
+      success: false,
+      mes: "Missing inputs...",
+    });
   }
-  const response = await User.create(req.body)
-  return res.status(200).json({
-    success: response ? true : false,
-    response
-  })
+  const user = await User.findOne({ email });
+  if (user) throw new Error("Email has existed");
+  else {
+    const newUser = await User.create(req.body);
+    return res.status(200).json({
+      success: newUser ? true : false,
+      mes: newUser ? "Register is successfully" : "Something is wrong",
+    });
+  }
+});
+
+const login = asyncHandler(async (req, res) => {
+  const { firstname, lastname, email, mobile, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      mes: "Missing inputs...",
+    });
+  }
+  const response = await User.findOne({ email });
+  if (response && await response.isCorrectPassword(password)){
+    const {password, role, ...userData} = response.toObject()
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+  }
+  else {
+    throw new Error('Mật khẩu không chính xác!')
+  }
 });
 
 module.exports = {
-    register
-}
+  register,
+  login
+};
