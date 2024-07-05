1572389634353282d9d0a10a15008c09cdd9a323
tienhnpl <dotien.21797@gmail.com>
2024-07-05 17:49:02 +0700

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,10 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
-const {generateAccessToken,generateRefreshToken} = require('../middlewares/jwt')
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../middlewares/jwt");
+const jwt = require("jsonwebtoken");
 
 const register = asyncHandler(async (req, res) => {
   const { firstname, lastname, email, mobile, password } = req.body;
@@ -30,36 +34,79 @@ const login = asyncHandler(async (req, res) => {
     });
   }
   const response = await User.findOne({ email });
-  if (response && await response.isCorrectPassword(password)){
-    const {password, role, ...userData} = response.toObject()
-    const accessToken = generateAccessToken(response._id,role)
-    const refreshToken = generateRefreshToken(response._id)
+  if (response && (await response.isCorrectPassword(password))) {
+    const { password, role, ...userData } = response.toObject();
+    const accessToken = generateAccessToken(response._id, role);
+    const refreshToken = generateRefreshToken(response._id);
     //lưu Token refresh vào database
-    await User.findByIdAndUpdate(response._id, {refreshToken}, {new:true})
+    await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true });
     //lưu token này vào cookies
-    res.cookie('refreshToken', refreshToken, {httpsOnly:true, maxAge: 7*24*60*60*1000})
-        return res.status(200).json({
-            success: true,
-            accessToken,
-            userData
-        })
-  }
-  else {
-    throw new Error('Mật khẩu không chính xác!')
+    res.cookie("refreshToken", refreshToken, {
+      httpsOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+      success: true,
+      accessToken,
+      userData,
+    });
+  } else {
+    throw new Error("Mật khẩu không chính xác!");
   }
 });
 
 const getCurrent = asyncHandler(async (req, res) => {
-        const {_id} = req.user
-        const user = await User.findById(_id).select('-refreshToken -password -role')
-        return res.status(200).json({
-            success: false,
-            rs: user ? user : 'User not found'
-        })
+  const { _id } = req.user;
+  const user = await User.findById(_id).select("-refreshToken -password -role");
+  return res.status(200).json({
+    success: false,
+    rs: user ? user : "User not found",
   });
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  // Kiểm tra xem có token ko
+  if (!cookie && !cookie.refreshToken)
+    throw new Error("No fresh token in cookies");
+  //check xem có hợp lệ hay ko
+  const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET);
+  //Check xem token có trùng với trong database ko
+  const response = await User.findOne({
+    _id: rs._id,
+    refreshToken: cookie.refreshToken,
+  });
+  return res.status(200).json({
+    success: response ? true : false,
+    newAccessToken: response
+      ? generateAccessToken(response._id, response.role)
+      : "Refresh token not matched",
+  });
+});
+
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie || !cookie.refreshToken)
+    throw new Error("No token refresh in cookies");
+  await User.findOneAndUpdate(
+    { refreshToken: cookie.refreshToken },
+    { refreshToken: "" },
+    { new: true }
+  );
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+  });
+  return res.status(200).json({
+    success: true,
+    mes: 'Logout is done'
+  })
+});
 
 module.exports = {
   register,
   login,
-  getCurrent
+  getCurrent,
+  refreshAccessToken,
+  logout,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -7,4 +7,6 @@ const {verifyAccessToken} = require('../middlewares/verifyToken')
 router.post('/register',ctrls.register)
 router.post('/login',ctrls.login)
 router.get('/current',verifyAccessToken,ctrls.getCurrent)
+router.post('/refreshtoken',ctrls.refreshAccessToken)
+router.get('/logout',ctrls.logout)
 module.exports = router
