1dc3e6a0c1ef3ef45689b04f8558fd9042f74d48
tienhnpl <dotien.21797@gmail.com>
2025-02-14 19:23:02 +0700

    bai lam

---
+++ Test_FinalAzure_250214/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Test_FinalAzure_250214.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
