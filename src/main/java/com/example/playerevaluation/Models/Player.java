ef7a7b31d8ac884882ad9f2cc279fa6c5315aa94
tienhnpl <dotien.21797@gmail.com>
2025-05-05 00:18:49 +0700

    bs

---
+++ src/main/java/com/example/playerevaluation/Models/Player.java
@@ -0,0 +1,11 @@
+package com.example.playerevaluation.Models;
+
+public class Player {
+    private int playerId;
+    private String name;
+    private String fullName;
+    private String age;
+    private int indexId;
+    private float value;
+    private String indexName;
+}
