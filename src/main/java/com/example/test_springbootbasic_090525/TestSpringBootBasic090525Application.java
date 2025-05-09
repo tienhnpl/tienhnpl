0f98275e5aee6e4cc1922e0c4f2a358defefcc28
tienhnpl <dotien.21797@gmail.com>
2025-05-09 19:12:22 +0700

    test

---
+++ src/main/java/com/example/test_springbootbasic_090525/TestSpringBootBasic090525Application.java
@@ -0,0 +1,13 @@
+package com.example.test_springbootbasic_090525;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootBasic090525Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootBasic090525Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_springbootbasic_090525/TestSpringBootBasic090525ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_springbootbasic_090525;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootBasic090525ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
