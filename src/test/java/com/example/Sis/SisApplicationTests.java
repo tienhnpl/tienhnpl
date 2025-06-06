53d394f659f817ea21ca2f93ce16ec4d1a26c750
tienhnpl <dotien.21797@gmail.com>
2025-06-06 19:15:29 +0700

    lan1

---
+++ src/test/java/com/example/Sis/SisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
